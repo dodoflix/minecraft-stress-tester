2fd11ccca4b37f7212a02630e5f3b54333eea947
dodoflix <dothish4nz@gmail.com>
2021-05-03 11:37:31 +0300

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # minecraft-server-attacker
-Using Mineflayer
+Minecraft server user spammer. I did cuz i bored nothing much here...
+
+# Features
+Spam players every server in every version.
+<br>Name randomizer.
+<br>Name prefix
+
+# TODO
+Proxy system.
+<br>Chat spammer.
+<br>Authentication.
+
+# HOW TO USE
+Install node js
+<br>Clone repository
+<br>Open terminal at repository folder
+<br>Type `npm install mineflayer`
+<br>Make configuration
+<br>And go! `node index.js`
--- index.js
+++ index.js
@@ -3,40 +3,50 @@ const client = require('mineflayer')
 /* CONFIGURATION */
 const serverIp = "127.0.0.1"
 const serverPort = "25565"
-const prefix = "dodoflix" // Null for empty
 const botCount = 20 // Inf for infinite
 const delay = 100 // Millisecond
+const version = 1.16
+
+/* MODULES */
+const prefix = "dodo" // Null for empty
+const nameRandomizer = true // After prefix
+//const chatSpammer = true // TODO
+//const authentication = true // TODO
+//const useProxy = true //TODO
 
 /* VARIABLES */
 botMap = new Map()
 let currentBotCount = 0
 
 /* MAIN */
-
 createClients()
 
-/*      */
-
-function createClients(){
-    if(currentBotCount < botCount){
+/* FUNCTIONS */
+function createClients() {
+    if (currentBotCount < botCount) {
         currentBotCount++
         setTimeout(() => {
-            botMap.set(botCount, client.createBot({
-                host: serverIp,
-                port: serverPort,
-                username: prefix + randomName(16 - prefix) // 16 is max for username
-            }))
-            createClients()
-            console.log("Bot " + botCount + " sent.")
+            createBot()
+            createClients() // Make it repeat
         }, delay)
     }
 }
 
-function randomName(length){
-    let result = [];
-    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let charactersLength = characters.length;
-    for (let i = 0; i < length; i++) {
+function createBot() {
+    botMap.set(botCount, client.createBot({
+        host: serverIp,
+        port: serverPort,
+        username: prefix + (nameRandomizer ? randomUserName(16 - prefix.length) : ""), // 16 is max for username
+        version: version
+    }))
+    console.log("Bot " + currentBotCount + " sent.")
+}
+
+function randomUserName(length) {
+    var result = [];
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
         result.push(characters.charAt(Math.floor(Math.random() *
             charactersLength)));
     }
