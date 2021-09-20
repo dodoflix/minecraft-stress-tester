eb96b72aa0d13e5554461bb37606a99dcbed07a1
dodoflix <dothish4nz@gmail.com>
2021-09-20 12:56:35 +0300

    Added anti-afk.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const {host, port} = require('./config.json');
 
 const {count} = require('./config.json') ?? 10;
 const {prefix} = require('./config.json');
+const {password} = require('./config.json') ?? "123123asD@";
 
 let bots = [];
 
@@ -18,12 +19,16 @@ for (let i = 0; i < count; i++) {
         host: host,
         port: port,
         username: `${prefix}${between(100, 999)}`,
+        plugins: [require('./modules/afk.js')],
+        verbose: true
     }));
 }
 
 bots.forEach(bot => {
     bot.once('spawn', () => {
-        bot.chat(`${bot.username} is here!`)
+        bot.chat(`${bot.username} is here!`);
+        bot.chat(`/register ${password} ${password}`);
+        bot.chat(`/login ${password}`);
     });
 
     bot.on('chat', (username, message) => {
@@ -31,6 +36,30 @@ bots.forEach(bot => {
         bot.chat(`${username}: ${message}`);
     });
 
-    bot.on('kicked', console.log);
-    bot.on('error', console.log);
+    bot.on('kicked', () => {
+        bots.push(mineflayer.createBot({
+            host: bot.host,
+            port: bot.port,
+            username: bot.username,
+            plugins: bot.plugins,
+            verbose: bot.verbose
+        }));
+        const index = bots.indexOf(bot);
+        if(index > -1) {
+            bots.splice(index, 1);
+        }
+    });
+    bot.on('error', () => {
+        bots.push(mineflayer.createBot({
+            host: bot.host,
+            port: bot.port,
+            username: bot.username,
+            plugins: bot.plugins,
+            verbose: bot.verbose
+        }));
+        const index = bots.indexOf(bot);
+        if(index > -1) {
+            bots.splice(index, 1);
+        }
+    });
 });
