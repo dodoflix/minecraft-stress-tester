093a48f7eec717a9e059c4d910e4bd673771321e
dodoflix <dothish4nz@gmail.com>
2021-09-20 12:00:19 +0300

    Version 0.2

--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
 const mineflayer = require('mineflayer');
 
-// Server utilities
 const {host, port} = require('./config.json');
 
-// Bot utilities
-const {count} = require('./config.json');
+const {count} = require('./config.json') ?? 10;
+const {prefix} = require('./config.json');
 
-const bot = mineflayer.createBot({
-    host: host,
-    port: port,
-    username: 'Test',
+let bots = [];
+
+function between(min, max) {
+    return Math.floor(
+        Math.random() * (max - min) + min
+    )
+}
+
+for (let i = 0; i < count; i++) {
+    bots.push(mineflayer.createBot({
+        host: host,
+        port: port,
+        username: `${prefix}${between(100, 999)}`,
+    }));
+}
+
+bots.forEach(bot => {
+    bot.once('spawn', () => {
+        bot.chat(`${bot.username} is here!`)
+    });
+
+    bot.on('chat', (username, message) => {
+        if (username.toString().startsWith(prefix)) return;
+        bot.chat(`${username}: ${message}`);
+    });
+
+    bot.on('kicked', console.log);
+    bot.on('error', console.log);
 });
-
-bot.on('chat', (username, message) => {
-    if (username === bot.username) return;
-    bot.chat(`${username}: ${message}`);
-});
-
-bot.on('kicked', console.log);
-bot.on('error', console.log);
